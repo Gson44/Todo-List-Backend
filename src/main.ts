a58ca7c221465ce0d40d456c69efda3ebcdc8622
Gerson <geric99997@gmail.com>
2025-01-01 11:35:15 -0600

    Add cors to github

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  app.enableCors({
+    origin: 'https://gson44.github.io', // or '*'
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+})
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
