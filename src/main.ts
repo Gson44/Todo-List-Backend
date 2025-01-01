45cc5ae671221e6c54bd61482257621bf8b06d02
Gerson <geric99997@gmail.com>
2025-01-01 10:09:49 -0600

    Fix code

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { NestFactory } from '@nestjs/core';
+/*import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -7,3 +7,21 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+*/
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+import * as express from 'express';
+
+const expressApp = express();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+  app.enableCors(); // Optional, if you need CORS
+  await app.init();
+}
+bootstrap();
+
+export const handler = (req, res) => {
+  expressApp(req, res);
+};
